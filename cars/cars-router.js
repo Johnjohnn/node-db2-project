90e755391f9928f41d4be21b432c0079c7a3fb51
Johnjohnn <conde2010@aol.com>
2020-10-18 22:43:28 -0400

    encountering issues

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -1,3 +1,18 @@
 const express = require("express")
 // an instance of knex connected to the database and ready to go 
 const db = require("../data/config")
+const knex = require("knex")
+
+const db = knex({
+	client: "sqlite3",
+	useNullAsDefault: true,
+	connection: {
+		filename: "./data/produce.db3",
+	},
+})
+
+const router = express.Router()
+
+
+
+module.exports = router
--- migrations/20201018212833_vehicules.js
+++ migrations/20201018212833_vehicules.js
@@ -2,7 +2,11 @@
 exports.up = async function(knex) {
   await knex.schema.createTable("cars", (table) => {
       // translates to "id INTEGER NOT NULL UNIQUE PRIMARY KEY"
-      table.integer("id").notNull().unique().primary()
+      // table.integer("id").notNull().unique().primary()
+      table.increments("id")
+      table.text("make").notNull()
+      table.text("model").notNull()
+      table.float("mileage").notNull()
   })
 };
 
